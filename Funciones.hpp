277b99f41c83d933ed60adad70780c7530ead892
diegoRecinos <00182122@uca.edu.sv>
2024-07-29 11:21:30 -0600

    final main version with characters

--- Funciones.hpp
+++ Funciones.hpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
+
 using namespace std;
 
 struct Heroe
 {
-    int salud = 400;
-    int ataque = 50;   
+    int salud = 600;
+    int ataque = 80;   
     float acc = 9; //si la suma de los dos dados es <= personaje.acc 
-    int heal = 10; //porcentaje que se puede curar el enemigo despues de un ataque
+    int heal = 40; //puntos que se puede curar el enemigo despues de un ataque
     int ataqueX2;
     float aciertoX2; // auxiliares  para el danio x2
 
@@ -19,15 +20,28 @@ struct Enemigo
     int salud;
     int ataque;
     float acc; //si la suma de los dos dados es <= enemigo.acc
-    int heal = 50; //porcentaje que se puede curar el enemigo despues de un ataque
+    int heal = 50; //puntos que se puede curar el enemigo despues de un ataque
 
 }enemigo1,enemigo2,enemigo3,enemigo4,enemigo5,
 enemigo6,enemigo7,enemigo8,enemigo9,jefefinal;
 
 //VARIABLES GLOBALES 
+int pos = 0; //posicion del jugador en el tablero
 
 bool muerto = false;
 
+//Representacion del tablero antes de comenzar la partida
+int tablero[15] = {0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 2, 3};
+/*
+0 1 1 1 0 1 1 1 0 1 1  1  0  2  3   
+"0" casillas sin nada, 
+"1" casillas de combate, 
+"2" casilla que te regresa a la primera posicion, 
+"3" casilla del jefe final )
+Posiciones: |0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|
+
+Tu posicion actual 0 
+*/
 
 
 
@@ -78,7 +92,6 @@ void Ataque(int *Salud, int *ataque, float *acc, bool turno) // a las funciones 
 {
 
     //Se muestra la probabilidad de acertar el ataque 
-
     cout << "\n\tProbabilidad de acierto: " << Calculo_Probabilidad(*acc) << "%" << endl;
     int dado1, dado2;
     bool acierto = false;
@@ -87,6 +100,7 @@ void Ataque(int *Salud, int *ataque, float *acc, bool turno) // a las funciones 
 
     cout << endl
          << "\tSe lanzan los dados y cae un " << dado1 << " y un " << dado2 << " (Suma debe de ser <= " << *acc << ")";
+
 
     // Se evalua si la suma de los dados cumple la condicion de acierto del enemigo correspondiente
     acierto = comprobar_Acierto(*acc, dado1, dado2);
@@ -105,12 +119,14 @@ void Ataque(int *Salud, int *ataque, float *acc, bool turno) // a las funciones 
             {
                 *Salud = 0;
             }
-            cout << *Salud << endl;
+            cout << *Salud << endl <<endl;
+            //system("pause");
         }
         else
         {
             cout << endl
-                 << "\tTu ataque fallo" << endl;
+                 << "\tTu ataque fallo" << endl <<endl;
+                 //system("pause");
         }
     }
     else
@@ -126,12 +142,14 @@ void Ataque(int *Salud, int *ataque, float *acc, bool turno) // a las funciones 
                 *Salud = 0;
             }
             
-            cout << *Salud << endl;
+            cout << *Salud << endl <<endl;
+            //system("pause");
         }
         else
         {
             cout << endl
-                 << "\tEl enemigo fallo su ataque" << endl;
+                 << "\tEl enemigo fallo su ataque" << endl <<endl;
+                 //system("pause");
         }
     }
 }
@@ -141,8 +159,8 @@ void curarse(int *Salud, int *heal , bool turno) // a las funciones se deben de 
 {
    if (turno == true) //turno del jugador
     {
-        int randInt = rand()%15+1; //funcion genera un numero aletorio de 1 a 10;
-        if ((randInt == 1)|| (randInt == 2)) // prob 2/15 de curarse
+        int randInt = rand()%15+1; //funcion genera un numero aletorio de 1 a 15;
+        if ((randInt == 1)|| (randInt == 2) || (randInt == 3)) // prob 3/15 de curarse
         {
         *Salud = *Salud +  *heal; //el personaje se cura el porcentaje de (salud * heal) 
         cout << "\tEl personaje se ha curado: " << *heal << " pts" <<endl;
@@ -153,7 +171,7 @@ void curarse(int *Salud, int *heal , bool turno) // a las funciones se deben de 
         int randInt = rand()%20+1; //funcion genera un numero aletorio de 1 a 20;
         if((randInt == 1)|| (randInt == 2) || (randInt == 3)) // prob 3/20 de curarse
         {
-        *Salud = *Salud + *heal; //el enemigo se cura el porcentaje de (salud * heal) 
+        *Salud = *Salud + *heal; //el enemigo se cura heal pts de salud) 
         cout << "\tEl enemigo se ha curado: " << *heal << " pts" <<endl;
         }
 
@@ -165,37 +183,91 @@ void combate(Heroe &personaje, Enemigo &enemigo)
 
     int accion;
     bool turno = true;
-
+    int enclavado = pos;
     // El combate durara hasta que los puntos de salud del Heroe o del enemigo sean 0
 
     while ((enemigo.salud > 0) && (personaje.salud > 0) )
     {
-
+        if ((enclavado == pos) || (tablero[pos] == 3)) // se evalua que enemigo va se va a enfrentar dependiendo de la posicion del jugador en el tablero
+        {                                               //se imprime en loop los personajes cada turno
+            if (tablero[pos] == 3)
+            {
+                enclavado = 12; //jefe
+            }
+            
+            switch (enclavado)
+            {
+            case 1:
+                    imprimirPersonaje();
+                    imprimirEnemigo1();
+                break;
+            case 2:
+                    imprimirPersonaje();
+                    imprimirEnemigo2();
+                break;
+            case 3:
+                    imprimirPersonaje();
+                    imprimirEnemigo3();
+                break;
+            case 5:
+                    imprimirPersonaje();
+                    imprimirEnemigo4();
+                break;
+            case 6:
+                    imprimirPersonaje();
+                    imprimirEnemigo5();
+                break;
+            case 7:
+                    imprimirPersonaje();
+                    imprimirEnemigo6();
+                break;
+            case 9:
+                    imprimirPersonaje();
+                    imprimirEnemigo7();
+                break;
+            case 10:
+                    imprimirPersonaje();
+                    imprimirEnemigo8();
+                break;
+            case 11:
+                    imprimirPersonaje();
+                    imprimirEnemigo9();
+                break;
+            case 12:
+                    imprimirPersonaje();
+                    imprimirJefeFinal();
+                break;           
+                        
+            default:
+                break;
+            }
+        }
+        
+        
         // Si turno = true, es el turno del jugador, de lo contrario es el turno del enemigo
         if (turno == true)
         {   
+            cout << "-------------TU TURNO------------" << endl;
             cout << endl;
             cout << "SALUD: " << personaje.salud << endl << "SALUD ENEMIGO: " << enemigo.salud << endl << "ATAQUE ENEMIGO: " << enemigo.ataque <<endl;
-
-            cout << endl;
-            cout<<endl;
-            cout<< "Es tu turno!" << endl;
-            cout << "Elige una accion: " << endl;
-            cout << "1. Ataque normal: " << personaje.ataque << " pts" << endl;
-            cout << "2. Ataque pesado (doble de danio, -20% Probabilidad de acertar)" << endl;
-            cout<<"opcion: ";
-
-            cin >> accion;
-            cout<<endl;
-
             bool continuar = true;
             do{
+                cout<<endl;
+                cout << "Elige una accion: " << endl;
+                cout << "1. Ataque normal: " << personaje.ataque << " pts" << endl;
+                cout << "2. Ataque pesado (doble de danio, -" << Calculo_Probabilidad(personaje.acc-3) <<"% " << "de acertar)" << endl;
+                cout<<"Opcion: ";
+
+                cin >> accion;
+                //cout<<endl;
+ 
                 switch (accion)
                 {
                 case 1:
                     Ataque(&enemigo.salud , &personaje.ataque, &personaje.acc, turno); // a las funciones se deben de evaluar en direcciones de memoria de variable: &var
                                                                                         //en la definicion de la funcion se ocupan punteros: *ptr
                     curarse(&personaje.salud , &personaje.heal, turno);
+                    system("pause");
                     // Cambio al turno del enemigo
                     turno = false; 
                     continuar = false; // booleano que interrumpe el do while 
@@ -203,15 +275,17 @@ void combate(Heroe &personaje, Enemigo &enemigo)
                 case 2:
                     // auxiliares para no modificar el original
                     personaje.ataqueX2 = personaje.ataque * 2; // danio de ataque x 2
-                    personaje.aciertoX2 = personaje.acc - 2.4; // % de acierto disminuye en un 20%
+                    personaje.aciertoX2 = personaje.acc - 3; // % de acierto disminuye en un 20%
 
                     Ataque(&enemigo.salud , &personaje.ataqueX2 , &personaje.aciertoX2, turno);
                     curarse(&personaje.salud , &personaje.heal, turno);
                     // Cambio al turno del enemigo
+                    system("pause");
                     turno = false;
                     continuar = false;
                     break;
                 default:
+
                     break;
                 }
             }while(continuar);
@@ -221,8 +295,10 @@ void combate(Heroe &personaje, Enemigo &enemigo)
         {
             if (enemigo.salud > 0)  //si el enemigo sigue en pie 
             {
+                cout << "-------------TURNO ENEMIGO-------------" << endl;
                 Ataque(&personaje.salud , &enemigo.ataque ,&enemigo.acc, turno); //enemigo ataca
                 curarse(&enemigo.salud , &enemigo.heal, turno); //el enemigo tiene cierta probabilida de curarse despues de un ataque
+                system("pause");
                 turno = true;// Cambio al turno del jugador
             }
         
--- RPG.cpp
+++ RPG.cpp
@@ -2,42 +2,32 @@
 #include <cstdlib>
 #include <time.h>
 #include <ctime>
-#include "Funciones.hpp"
 #include "Personajes.hpp"
+#include "Funciones.hpp"
 
 using namespace std;
-
-int pos = 0; //posicion del jugador en el tablero
-
 
 int main()
 {
     enemigo1.acc = 11, enemigo2.acc = 8, enemigo3.acc = 6 ; //suerte de los enemigos
     enemigo4.acc = 5, enemigo5.acc = 7, enemigo6.acc = 9; //si la suma de los dos dados es <= enemigoN.acc
     enemigo7.acc = 8, enemigo8.acc = 8, enemigo9.acc = 6;
-    jefefinal.acc = 10;
+    jefefinal.acc = 9;
 
     enemigo1.ataque = 25, enemigo2.ataque = 30, enemigo3.ataque = 40 ; //Puntos de da;o de los enemigos
     enemigo4.ataque = 50, enemigo5.ataque = 60, enemigo6.ataque = 70; 
-    enemigo7.ataque = 80, enemigo8.ataque = 90, enemigo9.ataque = 100;
-    jefefinal.ataque = 150;
+    enemigo7.ataque = 50, enemigo8.ataque = 60, enemigo9.ataque = 70;
+    jefefinal.ataque = 80;
+
 
     enemigo1.salud = 60, enemigo2.salud = 75, enemigo3.salud = 100 ; //Salud de enemigos
     enemigo4.salud = 150, enemigo5.salud = 150, enemigo6.salud = 175;
     enemigo7.salud = 175, enemigo8.salud = 175, enemigo9.salud = 175;
     jefefinal.salud = 500;
 
-    int tablero[15] = {0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 2, 3};
-
-    /*Representacion del tablero antes de comenzar la partida
-    0 1 1 1 0 1 1 1 0 1 1  1  0  2  3   (0 casillas sin nada, 1 casillas de combate, 2 casilla que te regresa a la primera posicion, 3 casilla del jefe final )
-    |0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|
-    Tu posicion actual 0 
-    */
-    
     int x;
     float probabilidadCombate;
-    int casosCombate;
+    float casosCombate;
     float dado;
     
     //El bucle acabara cuando el jugador llegue a la ultima posicion o cuando el jugador muera
@@ -52,18 +42,18 @@ int main()
             x++; //Incrementa las posiciones del tablero
             if ((tablero[x] == 1) || (tablero[x] == 3))
             {
-                casosCombate += 1; //Si el tablero en dicha posicion es 1 o 3 se sumara uno a los casos de combate
+                casosCombate++; //Si el tablero en dicha posicion es 1 o 3 se sumara uno a los casos de combate
             }
             
         }
-        //cout << "casosCombate: " << casosCombate <<endl;
+        
         //Para calcular la probabilidad de combate los casos favorables seran los casos de combate (Cuantas casillas de combate hay en las siguientes 3 posiciones)
         //entre los casos posibles que son 3 porque podes caer en tres posibles casillas, multiplicado por 100 para calcular el porcentaje
         probabilidadCombate =  casosCombate / 3 * 100;
         
         cout << endl;
         cout << "-------------------------------------------------------------------------" <<endl;
-        cout << "Tu posicion inicial: " << pos << "\t\t Probabilidad de combate: " << probabilidadCombate << "%" << endl;
+        cout << "Tu posicion inicial: " << pos << "\t\t\t Probabilidad de combate: " << probabilidadCombate << "%" << endl;
         dado = pos;
         pos += ((rand() % 3) + 1);//Genera un numero aleatorio entre 1-3 y se le suma a la posicion
         if (pos > 14)//Si la posicion mas el numero aleatorio da un numero mayor a 14, la posicion sera igual a 14 que es la ultima poscion del tablero
@@ -90,56 +80,38 @@ int main()
                 switch (pos) 
                 {
                 case 1:
-                    imprimirPersonaje();
-                    imprimirEnemigo1();
                     combate(personaje, enemigo1);
                     break;
                 
                 case 2: 
-                    imprimirPersonaje();
-                    imprimirEnemigo2();
                     combate(personaje, enemigo2);
                     break;
                 
                 case 3: 
-                    imprimirPersonaje();
-                    imprimirEnemigo3();
                     combate(personaje, enemigo3);
                     break;
 
                 case 5: 
-                    imprimirPersonaje();
-                    imprimirEnemigo4();
                     combate(personaje, enemigo4);
                     break;
 
                 case 6: 
-                    imprimirPersonaje();
-                    imprimirEnemigo5();
                     combate(personaje, enemigo5);
                     break;
 
                 case 7: 
-                    imprimirPersonaje();
-                    imprimirEnemigo6();
                     combate(personaje, enemigo6);
                     break;
 
                 case 9: 
-                    imprimirPersonaje();
-                    imprimirEnemigo7();
                     combate(personaje, enemigo7);
                     break;
 
                 case 10: 
-                    imprimirPersonaje();    
-                    imprimirEnemigo8();
                     combate(personaje, enemigo8);
                     break;
 
                 case 11: 
-                    imprimirPersonaje();
-                    imprimirEnemigo9();
                     combate(personaje, enemigo9);
                     break;
                 
@@ -147,7 +119,7 @@ int main()
                 default:
                     break;
                 }
-                
+                 tablero[pos] = 0;
             break;
         case 2:
             cout << "\nQue mala suerte! Tienes que empezar todo de nuevo" << endl;//2 casilla de reinicio
@@ -158,9 +130,9 @@ int main()
 
         case 3:
             cout << "\nEl jefe final se aproxima, ve a por el!" << endl;// 3 casilla de Jefe final
-            tablero[pos] = 0;
-            imprimirJefeFinal();
+            //imprimirJefeFinal();
             combate(personaje, jefefinal);
+            tablero[pos] = 0;
             break;
 
         default: 
@@ -168,7 +140,7 @@ int main()
         }
         
         cout << endl;
-        system("pause");
+        //system("pause");
     }
 
     if(muerto == true){//Si el jugador se murio saldra Game Over, sino saldra felicidades ...
@@ -176,4 +148,5 @@ int main()
     }else{
         cout<<" FELICIDADES HAS GANADO EL JUEGO "<<endl;
     }
+
 }
